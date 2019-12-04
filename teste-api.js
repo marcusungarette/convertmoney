639f5fc6131d15ecf28673f7d4bb98a308176d74
marcusungarette <marcusungarette@gmail.com>
2019-12-03 23:25:07 -0300

    Lesson 9

--- teste-api.js
+++ teste-api.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
 
-const url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='12-02-2019'&$top=100&$format=json&$select=cotacaoVenda'
+const url = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolar=====`
 
 axios
 .get(url)
 .then( res => console.log(res.data.value[0].cotacaoVenda))
+.catch( err => console.log(err))
